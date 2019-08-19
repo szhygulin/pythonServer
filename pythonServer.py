79e2c2955f9a4da2dd7d27b0c9bbd52af0afdcbe
Ubuntu <ubuntu@ip-172-31-90-204.ec2.internal>
2019-08-19 18:44:28 +0000

    first working server

--- pythonServer.py
+++ pythonServer.py
@@ -1,1 +1,31 @@
-import  http.server
+import http.server
+import socketserver
+from io import BytesIO
+
+PORT = 8000
+#Handler = http.server.SimpleHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+
+
