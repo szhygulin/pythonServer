4e330dca4b7817f9deefe454f5ed64368d5ce51b
Viacheslav Zhygulin <s.zhygulin@gmail.com>
2019-08-28 15:22:33 -0400

    added epoch voting

--- pythonServer.py
+++ pythonServer.py
@@ -7,6 +7,7 @@ PORT = 8000
 orders = {}
 cc_price = 0
 current_epoch = 0
+current_epoch_votes = 0
 #Handler = http.server.SimpleHTTPRequestHandler
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -23,6 +24,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         dict_obj['orders'] = orders
         #elif cur == 'orders':
         dict_obj['current_epoch'] = current_epoch
+        dict_obj['current_epoch_votes'] = current_epoch_votes
         #elif cur == 'current_epoch':
         b = json.dumps(dict_obj).encode('utf-8')
         self.wfile.write(b)
@@ -44,6 +46,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
         global current_epoch
         global cc_price
+        global current_epoch_votes
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -56,11 +59,14 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         cur_j = json.loads(body)
         print(cur_j)
         if "user_id" in cur_j:
-           orders[cur_j["user_id"]] = [cur_j["energy"],cur_j["usd"]]
+            orders[cur_j["user_id"]] = [cur_j["energy"],cur_j["usd"]]
         elif "current_epoch" in cur_j:
-           current_epoch = cur_j["current_epoch"]
+            current_epoch = cur_j["current_epoch"]
+            current_epoch_votes = 0
         elif "cc_price" in cur_j:
-           cc_price = cur_j["cc_price"]
+            cc_price = cur_j["cc_price"]
+        elif "current_epoch_votes" in cur_j:
+            current_epoch_votes += 1
 
 with socketserver.TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
     print("serving at port", PORT)
